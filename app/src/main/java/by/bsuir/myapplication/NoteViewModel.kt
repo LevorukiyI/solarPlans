ab3d35dfd3a6d4334943e28eed9f4216fce03332
LevorukiyI <92181087+LevorukiyI@users.noreply.github.com>
2023-11-10 18:28:54 +0300

    NoteViewModel Notes changed to Note

--- app/src/main/java/by/bsuir/myapplication/NoteViewModel.kt
+++ app/src/main/java/by/bsuir/myapplication/NoteViewModel.kt
@@ -3,16 +3,16 @@ package by.bsuir.myapplication
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
-import by.bsuir.myapplication.database.entity.Notes
+import by.bsuir.myapplication.database.entity.Note
 import by.bsuir.myapplication.database.entity.Weather
 
 class NoteViewModel(): ViewModel() {
-    val items: SnapshotStateList<Notes> = DefaultNotes.toMutableStateList()
+    val items: SnapshotStateList<Note> = DefaultNotes.toMutableStateList()
 
-    fun onClickRemoveNote(note: Notes) = items.remove(note)
+    fun onClickRemoveNote(note: Note) = items.remove(note)
 
     fun onClickAddNote(goal: String, date: String){
-        val newNote =  Notes(goal, date, Weather(19, 12, "30", "3"))
+        val newNote =  Note(goal, date, Weather(19, 12, "30", "3"))
         items.add(newNote)
     }
 
@@ -22,14 +22,14 @@ class NoteViewModel(): ViewModel() {
         note.date = date
     }
 
-    fun getItem(id: Int): Notes {
+    fun getItem(id: Int): Note {
         return items.get(id)
     }
 
     private companion object {
 
         private val DefaultNotes = listOf(
-            Notes("Make 3 PMIS labs", "12102023", Weather(19, 12, "30", "3"))
+            Note("Make 3 PMIS labs", "12102023", Weather(19, 12, "30", "3"))
         )
     }
 }
