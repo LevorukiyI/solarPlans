e80218637b3b68622be43a2b31b042bccbeeca1a
LevorukiyI <92181087+LevorukiyI@users.noreply.github.com>
2023-11-29 18:41:35 +0300

    pizda

--- app/src/main/java/by/bsuir/myapplication/screens/NewMainScreen.kt
+++ app/src/main/java/by/bsuir/myapplication/screens/NewMainScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.CircularProgressIndicator
 
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
@@ -22,44 +23,58 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import by.bsuir.myapplication.navigation.Screen
 import by.bsuir.vitaliybaranov.myapplication.R
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import by.bsuir.myapplication.DateDefaults
 
 import by.bsuir.myapplication.HomeViewModel
-import by.bsuir.myapplication.MaskVisualTransformation
 import by.bsuir.myapplication.Note
-import org.koin.android.ext.android.inject
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun HomeScreen(navController: NavController/*, viewModel: HomeViewModel*/){
     val viewModel = koinViewModel<HomeViewModel>()
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val viewState by viewModel._viewState.collectAsStateWithLifecycle()
     MaterialTheme {
-        Column(horizontalAlignment = Alignment.CenterHorizontally,modifier = Modifier.fillMaxSize()){
-                Button(onClick = {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,modifier = Modifier.fillMaxSize()) {
+            Button(
+                onClick = {
                     navController.navigate(Screen.AddScreen.route)
-                }, modifier = Modifier.padding(vertical = 16.dp), colors =  ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colorScheme.background, contentColor = MaterialTheme.colorScheme.primary)){
-                    Text(text = stringResource(id = R.string.addNote), fontSize = 20.sp, color = MaterialTheme.colorScheme.primary)
-                }
+                },
+                modifier = Modifier.padding(vertical = 16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.addNote),
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
             HomeScreenContent(
-                items = uiState.notes,
-                onEdit = { it -> navController.navigate(Screen.EditScreen.withArgs(it.id.toString()))},
+                items = viewState.notes,
+                onEdit = { it -> navController.navigate(Screen.EditScreen.withArgs(it.id.toString())) },
                 onRemove = {
                     viewModel.deleteNote(it)
                     navController.navigate(Screen.MainScreen.route)
                 },
                 navController = navController
             )
+            if (viewState.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
+            viewState.error?.let { error ->
+                Text(text = error.message ?: "Unknown error", color = Color.Red)
+            }
         }
 
     }
